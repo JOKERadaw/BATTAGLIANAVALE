fbb61bf7602a293c04b3327398f3123e14e486f6
JOKERadaw <65590227+JOKERadaw@users.noreply.github.com>
2022-03-25 18:16:46 +0100

    Update client.c

--- client.c
+++ client.c
@@ -4,7 +4,41 @@
 #include<sys/socket.h>
 #include<arpa/inet.h>	//inet_addr
 #include<unistd.h>	//write
-
+#define SUNK -1
+#define HIT 1
+#define MISS 0
+#define SQUARE_SIZE 50
+#define SPESSORECROCE 4
+// disegna una croce dati le coordinate logice (0-20,0-10)=(x,y) rossa=1 bianca=0
+void drawcross(int x,int y,int color){
+    int z;
+    gfx_color(255,255,255);
+    if(color)gfx_color(198,3,3);
+    for(z=0;z<SPESSORECROCE;z++){
+        gfx_line(x*SQUARE_SIZE+z,y*SQUARE_SIZE,x*SQUARE_SIZE+SQUARE_SIZE,y*SQUARE_SIZE+SQUARE_SIZE-z);
+        gfx_line(x*SQUARE_SIZE+z,y*SQUARE_SIZE+SQUARE_SIZE,x*SQUARE_SIZE+SQUARE_SIZE,y*SQUARE_SIZE+z);
+    }
+    for(z=1;z<SPESSORECROCE;z++){
+        gfx_line(x*SQUARE_SIZE,y*SQUARE_SIZE+z,x*SQUARE_SIZE+SQUARE_SIZE-z,y*SQUARE_SIZE+SQUARE_SIZE);
+        gfx_line(x*SQUARE_SIZE,y*SQUARE_SIZE+SQUARE_SIZE-z,x*SQUARE_SIZE+SQUARE_SIZE-z,y*SQUARE_SIZE);
+    }
+}
+//mattack ritorna se è stata affondata,mancata,beccata
+//e aggiorna la nostra matrice in base a questo
+//poichè anche noi vediamo quello che l'avversario ha colpito
+int mattack(int mat[][20],int barche[],int xy[]){
+    if(--barche[mat[xy[0]][xy[1]]]==0){
+        drawcross(xy[0],xy[1],1);
+        mat[xy[0]][xy[1]]=0;
+        return SUNK;
+    }else if(barche[mat[xy[0]][xy[1]]]>0){
+        drawcross(xy[0],xy[1],1);
+        mat[xy[0]][xy[1]]=0;
+        return HIT;
+    }
+    drawcross(xy[0],xy[1],0);
+    return MISS;
+}
 int connessione_client(char *ip, int porta);
 void play (int socket);
 
@@ -47,8 +81,10 @@ int main(int argc , char *argv[])
 
 
 void  play(int socket) {
-	char buffer[2000];
-	char message[] = "PONG";
+	int boats[]={1,1,1,1,2,2,2,3,3,4},mat[11][11]={0};
+	posboat(mat,boats);
+	int buffer[2];
+	int message;
 	int tocca_a_me=0;
 	int lenb=0;
 	for (int i =0; i<10; i++) {
@@ -58,6 +94,7 @@ void  play(int socket) {
 			write(socket , message , strlen(message));
 			tocca_a_me = 0;
 		}else {
+			
 			lenb =read(socket,buffer,100); //Bloccante, attende di ricevere i dati
 			buffer[lenb]='\0';
 			printf("Letti %d caratteri ---> ricevuto %s ",lenb, buffer);
