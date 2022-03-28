3f700851deaa6eb9b2d1d6d07d5de0ddd9ff15c1
JOKERadaw <65590227+JOKERadaw@users.noreply.github.com>
2022-03-28 14:38:39 +0200

    Update PARABELLIUM.c

--- PARABELLIUM.c
+++ PARABELLIUM.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <string.h>	
+#include <string.h>
 #include <stdlib.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
-#include<unistd.h>	
+#include<unistd.h>
 #include <time.h>
 #include "multiplayer.h"
 #include "bn_graphics.h"
@@ -11,32 +11,32 @@
 #include "control.h"
 #define CLIENT 1
 #define SERVER 0
-void  play(int socket,int turno);
+int  play(int socket,int turno);
 
 int id;
 
 int main(int argc , char *argv[])
 {
-	int socket_desc;
-	int porta;
-	int turno;
+    int socket_desc;
+    int porta;
+    int turno;
     char t[2];
-	srand(time(NULL));
+    srand(time(NULL));
     if(argc ==2){
-	    porta = atoi(argv[1]);
-    	socket_desc=connessione_server(porta);
-		turno = rand()%2;
+        porta = atoi(argv[1]);
+        socket_desc=connessione_server(porta);
+        turno = rand()%2;
         id = SERVER;
         sprintf(t,"%d",turno);
         write(socket_desc,t,1);
     }else if(argc == 3){
-	    porta = atoi(argv[2]);
-	    socket_desc=connessione_client(argv[1],porta);
+        porta = atoi(argv[2]);
+        socket_desc=connessione_client(argv[1],porta);
         id = CLIENT;
     }else{
         perror("specificare la porta di esecuzione 1025-32000 ...Esempio  $./server 8888(Server)\n");
         perror("specificare indirizzo e porta del server ...Esempio  $./server 127.0.0.1 8888(Client)\n");
-		return -1;
+        return -1;
     }
     if(id == CLIENT){
         int len =read(socket_desc,t,1);
@@ -44,17 +44,17 @@ int main(int argc , char *argv[])
         turno = atoi(t);
     }
     play(socket_desc,turno);
-	close(socket_desc);
-	return 0;
+    close(socket_desc);
+    return 0;
 }
 
 
-void  play(int socket,int turno) {
-	char buffer[2000],cordinate_colpite[4],risposta[1024];
+int  play(int socket,int turno) {
+    char buffer[2000],cordinate_colpite[4],risposta[1024];
     int cord[2];
-	int lenb;
-	int mouse_pos_x,mouse_pos_y;
-	int event = 0;
+    int lenb;
+    int mouse_pos_x,mouse_pos_y;
+    int event = 0;
     int grid[ROW][COLUMN];
     memset(grid,0,ROW*COLUMN*sizeof(int));
     int boats[]={1,1,1,1,2,2,2,3,3,4};
@@ -62,11 +62,12 @@ void  play(int socket,int turno) {
     int valido = 0;
     int posiziona =1;
     int r,c;
+    int vinto;
     gfx_open(WINDOW_WIDTH,WINDOW_HEIGHT,"Battaglia Navale");
     gfx_color(255,255,255);
     draw_grid(ROW,COLUMN,50);
     set_text("E' il tuo turno premi tasto destro per inserire una nave in verticale e sinistro in orizzontale");
-        
+
    while (event != 'q'){
         if(posiziona == 1){
             event = gfx_wait();
@@ -80,7 +81,7 @@ void  play(int socket,int turno) {
             }else if((int)event == 3 && size < 10 && posiziona == 1){
                 insert_ship(grid,c,r,mouse_pos_x,mouse_pos_y,VERTICAL,boats,&size);
             }
-            
+
             if(size >= 10 && posiziona == 1){
                 set_text("Se sei pronto premi il tasto p");
             }
@@ -94,9 +95,11 @@ void  play(int socket,int turno) {
             }
         }else{
             if(id == turno){
+                if(vinto)return 0;
                 set_text("Premi la casella da colpire del nemico");
                 event = gfx_wait();
                 if((int) event == 1){
+                    gfx_line_width(1);
                     mouse_pos_x = gfx_xpos();
                     mouse_pos_y = gfx_ypos();
                     c=xtocolumn(mouse_pos_x);
@@ -109,9 +112,13 @@ void  play(int socket,int turno) {
                         turno = !turno;
                         lenb = read(socket,risposta,1024);
                         risposta[lenb] = '\0';
-						if(risposta[0]=='M')drawcross(c,r,0);
-						else drawcross(c,r,1);
-						set_text(risposta);
+                        if(risposta[0]=='h'){
+                            drawcross(c,r,1);
+                            return 0;
+                        }
+                        if(risposta[0]=='M')drawcross(c,r,0);
+                        else drawcross(c,r,1);
+                        set_text(risposta);
                         gfx_flush();
                     }
                 }
@@ -120,24 +127,42 @@ void  play(int socket,int turno) {
                 cordinate_colpite[lenb] = '\0';
                 turno = !turno;
                 sscanf(cordinate_colpite,"%d %d",&cord[0],&cord[1]);
+                gfx_line_width(1);
                 if(grid[cord[1] - 1][cord[0]-10] > 0){
-                    strcpy(risposta,"Colpita");
+                    drawcross(cord[0]-10,cord[1],1);
+
+                    grid[cord[1]-1][cord[0]-10]=0;
+                    vinto=1;
+                    for(int x=0;x<10;x++){
+                    for(int y=0;y<10;y++){
+                        if(grid[x][y]>0){
+                            vinto=0;break;
+                        }
+                    }
+                    if(!vinto)break;
+                    }
+                    if(vinto){
+                        strcpy(risposta,"hai vinto");
+                    }else{
+                        strcpy(risposta,"Colpita");
+                    }
                 }else{
+                    drawcross(cord[0]-10,cord[1],0);
                     strcpy(risposta,"Mancata");
                 }
-				for(int x=0;x<20;x++){
-				
-					for(int y=0;y<10;y++){
-						printf("%d ",grid[x][y]);
-					}
-					printf("\n");
-				}
+                for(int x=0;x<20;x++){
+
+                    for(int y=0;y<10;y++){
+                        printf("%d ",grid[x][y]);
+                    }
+                    printf("\n");
+                }
                 write(socket,risposta,strlen(risposta));
 
                 set_text("Premi la casella da colpire del nemico");
             }
         }
 
-        
+
     }
 }
